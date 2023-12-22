15f0bf6d3fe292fcdbc1193675b54616b2d240b2
John Coonrod <john@mcld.org>
2023-12-22 12:03:57 -0500

    debug edit

--- app/edit.php
+++ app/edit.php
@@ -11,10 +11,11 @@ $page=new \Thpglobal\Classes\Page;
 $page->start("$prefix in $table");
 if($table=='') Die("No table set.");
 $form=new \Thpglobal\Classes\Form;
+$hidden_fields="id".$_COOKIE["hidden"]; // string like "field1,field2";
+$form->debug("Hidden fields",$hidden_fields);
 $form->start($db,"/update");
-$hidden_fields=$_COOKIE["hidden"]; // string like "field1,field2";
 // the values are in cookies by those names
-if($hidden_fields) $form->hidden=explode("id".$hidden_fields,",");
+if($hidden_fields) $form->hidden=explode($hidden_fields,",");
 $form->record($table,$id);
 $form->end("Save data");
 $page->end();
