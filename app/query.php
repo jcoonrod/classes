87fddbb8158bcc48761b2b7a9bbdb1c887f13f68
John Coonrod <54367582+jcoonrod@users.noreply.github.com>
2024-11-21 11:46:00 -0500

    Update query.php

--- app/query.php
+++ app/query.php
@@ -1,36 +1,5 @@
-<?php
-// Simple query display
-$page=new Thpglobal\Classes\Page;
-$page->icon("download","/export","Download as excel");
-$page->icon("pie-chart","chart?chart_type=pie","Display as Pie Chart");
-$page->icon("bar-chart","chart?chart_type=bar","Display as Bar Chart");
-$page->icon("line-chart","chart?chart_type=line","Display as Line Chart");
-$page->start("Query");
-?>
-<p><button onclick="showtables(); ">Show tables</button> 
-<button onclick="goback()">&lt;</button> 
-<button onclick="gofwd()">&gt;</button> 
-<button onclick="goclear()"><span class='fa fa-trash'></span></button> 
-<span id=nq>0</span> queries stored. Loading <span id=iq>0</span>.</p>
-<?php
-$query=$_GET["query"]??'';
-echo("<form><textarea id=q name=query rows=3 cols=80>$query</textarea><input type=submit onclick='saveq()'></form>\n");
-if($query){
-	$start=substr($query,0,4); // determine type of event based on first 4 letters
-	if(in_array($start,array("show","sele","expl"))){
-		$grid=new \Thpglobal\Classes\Table;
-		$grid->start($db);
-		$grid->query($query);
-		$grid->show();
-	}else{
-    if(!ADMIN) Die("Not authorized");
-    require_once($_SERVER["DOCUMENT_ROOT"]."/includes/root.php");
-    $count=$db->exec($query);
-    echo("<p>Rows affected: $count</p>");
-	}
-}
-?>
 <!-- The following are used to store past copies of the query in local storage -->
+<!-- Moved to top in case there is a error in the query -->
 <script>
   var queries = [];
   if (!localStorage.queries)
@@ -72,5 +41,38 @@ function showni(){
   document.getElementById('iq').textContent=i;
 }
 </script>
+<?php
+// Simple query display
+$page=new Thpglobal\Classes\Page;
+$page->icon("download","/export","Download as excel");
+$page->icon("pie-chart","chart?chart_type=pie","Display as Pie Chart");
+$page->icon("bar-chart","chart?chart_type=bar","Display as Bar Chart");
+$page->icon("line-chart","chart?chart_type=line","Display as Line Chart");
+$page->start("Query");
+?>
+<p><button onclick="showtables(); ">Show tables</button> 
+<button onclick="goback()">&lt;</button> 
+<button onclick="gofwd()">&gt;</button> 
+<button onclick="goclear()"><span class='fa fa-trash'></span></button> 
+<span id=nq>0</span> queries stored. Loading <span id=iq>0</span>.</p>
+<?php
+$query=$_GET["query"]??'';
+echo("<form><textarea id=q name=query rows=3 cols=80>$query</textarea><input type=submit onclick='saveq()'></form>\n");
+if($query){
+	$start=substr($query,0,4); // determine type of event based on first 4 letters
+	if(in_array($start,array("show","sele","expl"))){
+		$grid=new \Thpglobal\Classes\Table;
+		$grid->start($db);
+		$grid->query($query);
+		$grid->show();
+	}else{
+    if(!ADMIN) Die("Not authorized");
+    require_once($_SERVER["DOCUMENT_ROOT"]."/includes/root.php");
+    $count=$db->exec($query);
+    echo("<p>Rows affected: $count</p>");
+	}
+}
+?>
+
 <?php 
 $page->end();
