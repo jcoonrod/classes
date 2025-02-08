54033b054ac1e54ed1abc4d14a85772d81c16a55
John Coonrod <54367582+jcoonrod@users.noreply.github.com>
2025-02-08 11:40:22 -0500

    Update cookies.php

--- app/cookies.php
+++ app/cookies.php
@@ -3,7 +3,7 @@
 $page=new \Thpglobal\Classes\Page;
 $page->start("Cookies");
 echo("<h2>Constants</h1>\n");
-echo("<pre>".print_r(get_defined_constants(TRUE)["user"],TRUE)."</pre>\n");
+echo("<pre>".print_r(get_defined_constants(TRUE)["user"]??'',TRUE)."</pre>\n");
 echo("<pre>".print_r($_COOKIE,TRUE)."</pre>");
 echo("<h2>Post</h2>\n<pre>".print_r($_POST,TRUE)."</pre>\n");
 $page->end();
